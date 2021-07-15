29d2faeba9399f47b4906f5e15d5cbb24d4b6a95
Kristian Kutin <kristian.kutin@arcor.de>
2021-07-15 23:26:04 +0200

    Update
    1) Added a feature to normalize Java strings

---
+++ Utilities/String-Tests/src/test/jmul/string/NormalizeJavaStringValidParametersTest.java
@@ -0,0 +1,135 @@
+/*
+ * SPDX-License-Identifier: GPL-3.0
+ *
+ *
+ * (J)ava (M)iscellaneous (U)tilities (L)ibrary
+ *
+ * JMUL is a central repository for utilities which are used in my
+ * other public and private repositories.
+ *
+ * Copyright (C) 2021  Kristian Kutin
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * e-mail: kristian.kutin@arcor.de
+ */
+
+/*
+ * This section contains meta informations.
+ *
+ * $Id$
+ */
+
+package test.jmul.string;
+
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import jmul.string.TextNormalizationHelper;
+
+import jmul.test.classification.UnitTest;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+
+/**
+ * Tests the text normalization helper, i.e. the normalization of an XML string with valid
+ * test data.
+ *
+ * @author Kristian Kutin
+ */
+@UnitTest
+@RunWith(Parameterized.class)
+public class NormalizeJavaStringValidParametersTest {
+
+    /**
+     * The actual input.
+     */
+    private String input;
+
+    /**
+     * The expected result.
+     */
+    private String expectedResult;
+
+    /**
+     * Creates a new test case according to the specified characters.
+     *
+     * @param anInput
+     *        the string which is to be normalized
+     * @param anExpectedResult
+     *        the expected normalization result
+     */
+    public NormalizeJavaStringValidParametersTest(String anInput, String anExpectedResult) {
+
+        super();
+
+        input = anInput;
+        expectedResult = anExpectedResult;
+    }
+
+    /**
+     * Tests the normalization and checks the results.
+     */
+    @Test
+    public void testNormalizeXmlString() {
+
+        String actualResult = TextNormalizationHelper.toNormalizedJavaString(input);
+
+        String message =
+            String.format("The input (\"%s\") has been transformed (\"%s\") but another result is expected (\"%s\")!",
+                          input, actualResult, expectedResult);
+        assertEquals(message, expectedResult, actualResult);
+    }
+
+    /**
+     * Returns a matrix of input data.
+     *
+     * @return a matrix of input data
+     */
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+
+        Collection<Object[]> parameters = new ArrayList<Object[]>();
+
+        parameters.add(new Object[] { null, null });
+        parameters.add(new Object[] { "", "" });
+        parameters.add(new Object[] { "a", "a" });
+        parameters.add(new Object[] { "ab", "ab" });
+        parameters.add(new Object[] { "a\"b", "a\"b" });
+        parameters.add(new Object[] { "a'b", "a'b" });
+        parameters.add(new Object[] { "B\u00e4umb\u00f6rd\u00fcre", "B&#228;umb&#246;rd&#252;re" });
+        parameters.add(new Object[] { "\u00c4", "&#196;" });
+        parameters.add(new Object[] { "\u00e4", "&#228;" });
+        parameters.add(new Object[] { "\u00d6", "&#214;" });
+        parameters.add(new Object[] { "\u00f6", "&#246;" });
+        parameters.add(new Object[] { "\u00dc", "&#220;" });
+        parameters.add(new Object[] { "\u00fc", "&#252;" });
+        parameters.add(new Object[] { "\u00df", "&#223;" });
+        parameters.add(new Object[] { "<", "<" });
+        parameters.add(new Object[] { ">", ">" });
+        parameters.add(new Object[] { "--", "--" });
+        parameters.add(new Object[] { "-", "-" });
+        parameters.add(new Object[] { "Hello\r\nWorld!", "Hello\r\nWorld!" });
+        parameters.add(new Object[] { "Hello\rWorld!", "Hello\rWorld!" });
+        parameters.add(new Object[] { "Hello\nWorld!", "Hello\nWorld!" });
+
+        return parameters;
+    }
+
+}
--- Utilities/String/src/jmul/string/TextNormalizationHelper.java
+++ Utilities/String/src/jmul/string/TextNormalizationHelper.java
@@ -57,6 +57,11 @@ public final class TextNormalizationHelper {
      */
     private static final Map<String, String> STRING_TO_XML_STRING_CHARACTER_MAPPINGS;
 
+    /**
+     * A mapping for special characters.
+     */
+    private static final Map<String, String> STRING_TO_JAVA_STRING_CHARACTER_MAPPINGS;
+
     /*
      * The static initializer.
      */
@@ -65,7 +70,9 @@ public final class TextNormalizationHelper {
         FALLBACK_CHARACTER = "\ufffd";
 
 
-        Map<String, String> tmp = new HashMap<>();
+        Map<String, String> tmp;
+
+        tmp = new HashMap<>();
 
         // characters that need to be escaped
         tmp.put("&", "&amp;");
@@ -94,6 +101,18 @@ public final class TextNormalizationHelper {
 
 
         STRING_TO_XML_STRING_CHARACTER_MAPPINGS = Collections.unmodifiableMap(tmp);
+
+
+        tmp = new HashMap<>();
+        tmp.put("\u00c4", "\\u00c4"); // AE
+        tmp.put("\u00e4", "\\u00e4"); // ae
+        tmp.put("\u00d6", "\\u00d6"); // OE
+        tmp.put("\u00f6", "\\u00f6"); // oe
+        tmp.put("\u00dc", "\\u00dc"); // UE
+        tmp.put("\u00fc", "\\u00fc"); // ue
+        tmp.put("\u00df", "\\u00df"); // sz
+
+        STRING_TO_JAVA_STRING_CHARACTER_MAPPINGS = Collections.unmodifiableMap(tmp);
     }
 
     /**
@@ -108,7 +127,7 @@ public final class TextNormalizationHelper {
      * When writing an XML file string values should be normalized, i.e. some
      * characters need to be escaped (e.g. &gt; or &lt;) and some language
      * dependent special character need to be replaced to preserve the actual character
-     * (e.g. in german this applies to &#196;, &#214;, &#220;, etc.).When the output file
+     * (e.g. in german this applies to &#196;, &#214;, &#220;, etc.). When the output file
      * has a lesser encoding (e.g. UTF-16 to UTF-8) there is some loss.<br>
      * Additionally there exist combinations of characters that make the XML file not well formed (e.g. '--').<br>
      * <br>
@@ -132,7 +151,7 @@ public final class TextNormalizationHelper {
 
     /**
      * This operation recurses into the specified string and replaces certain characters in order to
-     * preserve the characters when exporting to XML.
+     * preserve the characters when exporting to an XML file.
      *
      * @param theFullCharSequence
      *        the full string is needed for error messages
@@ -209,6 +228,112 @@ public final class TextNormalizationHelper {
         CharSequence remainder = aCharSequence.subSequence(1, length);
 
         return first + toNormalizedXmlString(theFullCharSequence, remainder);
+    }
+
+
+    /**
+     * When writing a text file string values should be normalized, i.e. some
+     * language dependent special character need to be replaced to preserve the actual character
+     * (e.g. in german this applies to &#196;, &#214;, &#220;, etc.). When the output file
+     * has a lesser encoding (e.g. UTF-16 to UTF-8) there is some loss.<br>
+     * <br>
+     * This operation returns a normalized string where some of these issues are resolved.<br>
+     * <br>
+     * <i>Note:<br>
+     * Update the operation as needed when new cases of problematic characters have been
+     * identified.<br>
+     * The current implementation may not suit a language independent solution. Change the implementation
+     * as needed.</i>
+     *
+     * @param aCharSequence
+     *        a string which is not normalized
+     *
+     * @return a normalized string
+     */
+    public static String toNormalizedJavaString(CharSequence aCharSequence) {
+
+        return toNormalizedJavaString(aCharSequence, aCharSequence);
+    }
+
+    /**
+     * This operation recurses into the specified string and replaces certain characters in order to
+     * preserve the characters when exporting to a text file.
+     *
+     * @param theFullCharSequence
+     *        the full string is needed for error messages
+     * @param aCharSequence
+     *        the string which is normalized
+     *
+     * @return a normalized string
+     */
+    private static String toNormalizedJavaString(CharSequence theFullCharSequence, CharSequence aCharSequence) {
+
+        if (aCharSequence == null) {
+
+            return null;
+        }
+
+        if (aCharSequence.length() == 0) {
+
+            return String.valueOf(aCharSequence);
+        }
+
+
+        // Check for unrecognized characters. If such a character is found then the string
+        // contains an illegal character. At this point it's not possible to restore or map
+        // the actual character.
+        {
+            char c1 = aCharSequence.charAt(0);
+            char c2 = FALLBACK_CHARACTER.charAt(0);
+
+            if (c1 == c2) {
+
+                String message =
+                    String.format("The specified string (%s) contains at least one unrecognized character (%s)!",
+                                  theFullCharSequence, c2);
+                throw new IllegalArgumentException(message);
+            }
+        }
+
+
+        int length = aCharSequence.length();
+
+        for (String token : STRING_TO_JAVA_STRING_CHARACTER_MAPPINGS.keySet()) {
+
+            int tokenLength = token.length();
+
+            boolean match = true;
+            for (int i = 0; i < tokenLength; i++) {
+
+                char c1 = token.charAt(i);
+
+                if (i >= length) {
+
+                    match = false;
+                    break;
+                }
+
+                char c2 = aCharSequence.charAt(i);
+
+                match = match && c1 == c2;
+            }
+
+            if (!match) {
+
+                continue;
+            }
+
+            CharSequence remainder = aCharSequence.subSequence(token.length(), length);
+            String replacementToken = STRING_TO_XML_STRING_CHARACTER_MAPPINGS.get(token);
+
+            return replacementToken + toNormalizedJavaString(theFullCharSequence, remainder);
+        }
+
+
+        char first = aCharSequence.charAt(0);
+        CharSequence remainder = aCharSequence.subSequence(1, length);
+
+        return first + toNormalizedJavaString(theFullCharSequence, remainder);
     }
 
 }
